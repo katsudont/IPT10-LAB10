bf096c15dc6cbf5b8d6209654846a96e0437329d
jbernardo <92368947+katsudont@users.noreply.github.com>
2024-10-18 22:56:06 +0800

    Delete add-to-cart.php

--- add-to-cart.php
+++
@@ -1,8 +0,0 @@
-<? php
-// Add to cart logic
-if ($_SESSION['REQUEST_METHOD'] == 'POST' && isset($_POST['product_id'])) {
-    $product_id = $_POST['product_id'];
-    // TODO: Add product to cart (Session variable), try array_push()...
-}
-// Redirect to the products browsing page
-header(“Location: cart.php”);
